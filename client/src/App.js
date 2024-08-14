fd94e438acebf417af53a11d54e656e98790c614
Karan Bisht <karan161003@gmail.com>
2024-08-14 21:42:17 +0530

    new file:   .gitignore 	modified:   client/.env 	modified:   client/.gitignore 	modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/Actions.js 	modified:   client/src/App.css 	modified:   client/src/App.js 	deleted:    client/src/App.test.js 	modified:   client/src/Socket.js 	modified:   client/src/components/Client.js 	modified:   client/src/components/Editor.js 	modified:   client/src/components/EditorPage.js 	modified:   client/src/components/Home.js 	modified:   client/src/components/Whiteboard.jsx 	modified:   client/src/index.css 	modified:   client/src/index.js 	deleted:    client/src/reportWebVitals.js 	deleted:    client/src/setupTests.js 	new file:   locofy 	new file:   server/.gitignore 	modified:   server/Actions.js 	modified:   server/index.js 	modified:   server/node_modules/.package-lock.json 	modified:   server/package-lock.json 	modified:   server/package.json

--- client/src/App.js
+++ client/src/App.js
@@ -6,29 +6,29 @@ import { Toaster } from "react-hot-toast";
 import Whiteboard from "./components/Whiteboard";
 
 function App() {
-  return (
-    <>
-      <div>
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            success: {
-              theme: {
-                primary: "#4aed88",
-              },
-            },
-          }}
-        ></Toaster>
-      </div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/editor/:roomId" element={<EditorPage />}></Route>
-          <Route path="/whiteboard/:roomId" element={<Whiteboard/>} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <Toaster
+                    position="top-right"
+                    toastOptions={{
+                        success: {
+                            theme: {
+                                primary: "#4aed88",
+                            },
+                        },
+                    }}
+                ></Toaster>
+            </div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/editor/:roomId" element={<EditorPage />}></Route>
+                    <Route path="/whiteboard/:roomId" element={<Whiteboard />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -6,52 +6,53 @@ import "codemirror/addon/edit/closebrackets";
 import "codemirror/lib/codemirror.css";
 import CodeMirror from "codemirror";
 import { ACTIONS } from "../Actions";
+
 const Editor = ({ socketRef, roomId, onCodeChange }) => {
-  const editorRef = useRef(null);
+    const editorRef = useRef(null);
 
-  useEffect(() => {
-    async function init() {
-      editorRef.current = CodeMirror.fromTextArea(
-        document.getElementById("realtimeEditor"),
-        {
-          mode: { name: "javascript", json: true },
-          theme: "dracula",
-          autoCloseTags: true,
-          autoCloseBrackets: true,
-          lineNumbers: true,
+    useEffect(() => {
+        async function init() {
+            editorRef.current = CodeMirror.fromTextArea(
+                document.getElementById("realtimeEditor"),
+                {
+                    mode: { name: "javascript", json: true },
+                    theme: "dracula",
+                    autoCloseTags: true,
+                    autoCloseBrackets: true,
+                    lineNumbers: true,
+                }
+            );
+
+            editorRef.current.on("change", (instance, changes) => {
+                const { origin } = changes;
+                const code = instance.getValue();
+                onCodeChange(code);
+                if (origin !== "setValue") {
+                    socketRef.current.emit(ACTIONS.CODE_CHANGE, {
+                        roomId,
+                        code,
+                    });
+                }
+            });
         }
-      );
+        init();
+    }, []);
 
-      editorRef.current.on("change", (instance, changes) => {
-        const { origin } = changes;
-        const code = instance.getValue();
-        onCodeChange(code);
-        if (origin !== "setValue") {
-          socketRef.current.emit(ACTIONS.CODE_CHANGE, {
-            roomId,
-            code,
-          });
+    useEffect(() => {
+        if (socketRef.current) {
+            socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
+                if (code != null) {
+                    editorRef.current.setValue(code);
+                }
+            });
         }
-      });
-    }
-    init();
-  }, []);
+        return () => {
+            socketRef.current.off(ACTIONS.CODE_CHANGE);
+        };
+    }, [socketRef.current]);
 
-  useEffect(() => {
-    if (socketRef.current) {
-      socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
-        if (code != null) {
-          editorRef.current.setValue(code);
-        }
-      });
-    }
-    return () => {
-      socketRef.current.off(ACTIONS.CODE_CHANGE);
-    };
-  }, [socketRef.current]);
-
-  return <textarea id="realtimeEditor"></textarea>;
+    return <textarea id="realtimeEditor"></textarea>;
 };
-  
+
 
 export default Editor;
